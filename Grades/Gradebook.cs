b6c7b2b96edcf0248da8c4ab223bc8a82418f6eb
MOTORCREDITO\ebalbuena <eddy.rodney.balbuena@gmail.com>
2018-05-25 20:12:04 -0400

    initial commit.

---
+++ Grades/Gradebook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Gradebook
+    {
+        public Gradebook()
+        {
+          
+        }
+
+        List<float> grades = new List<float>();
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+
+            foreach (var grade in grades)
+            {
+                stats.HighestGade = Math.Max(grade, stats.HighestGade);
+                stats.LowestGrade = Math.Min(grade, stats.HighestGade);
+
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;           
+            return stats;
+        }
+
+        
+
+        public void AddGrade(float grade) => grades.Add(grade); 
+    }
+}
